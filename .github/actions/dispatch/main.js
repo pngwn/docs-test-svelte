305e80ec3265b1dc7b58b185978aef598f2edc47
pngwn <hello@pngwn.io>
2021-03-16 22:41:12 +0000

    action

--- .github/actions/dispatch/main.js
+++ .github/actions/dispatch/main.js
@@ -3,6 +3,20 @@ import github from "@actions/github";
 
 function run() {
 	console.log(JSON.stringify(github, null, 2));
+	const {context: { eventName, payload } } = github;
+
+	switch (eventName) {
+		case 'release':
+
+			console.log(`new release for ${payload.release.tag_name}`)
+			break;
+		case 'push':
+			
+			console.log(`unreleased docs`);
+			break;
+		default:
+			break;
+	}
 
 
 	
